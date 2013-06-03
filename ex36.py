090543db9db16426a099f7a748734d6c5643ab6d
lsprague <lily.sprague@gmail.com>
2013-06-02 20:54:41 -0700

    nearly complete

--- ex36.py
+++ ex36.py
@@ -2,14 +2,18 @@
 
 from sys import exit
 
+# specific blood color will matter for one ending; passing it out of the function where
+# the user chooses it.
+blood = []
+
 def start():
     print '''After the grueling savagery of the trials, you're close to finding a lusus at
 last. Now you just need to track down a beast appropriate to your hemotype.
 What IS your hemotype, anyway?
-(acceptable answers include rust, brown, yellow, arsenic, jade, teal, cerulean,
+(acceptable answers include rust, brown, yellow, olive, jade, teal, cerulean,
 blue, indigo, violet, fuschia)'''
     hemotype = raw_input("> ")
-    landtypes = ["rust", "brown", "yellow", "arsenic", "jade", "teal", "cerulan", "blue", "indigo"]
+    landtypes = ["rust", "brown", "yellow", "olive", "jade", "teal", "cerulan", "blue", "indigo"]
     
     if hemotype == "violet" or hemotype == "fuschia":
         print "A seadweller! Into the pool you go."
@@ -17,7 +21,12 @@ blue, indigo, violet, fuschia)'''
         print "Ah, the sea. Your rightful heritage!"
         seastart()
     elif hemotype in landtypes:
+        blood.append(hemotype)
         print "A landdweller. Shoo, out through the caves with you."
+        print "You are in a cave. Nothing new there! But now you can explore DIFFERENT caves."
+        print "There are tunnels to your left and right. The left is dark. The right is..."
+        print "Well, also dark. But not AS dark. There's probably light in the distance."
+        landstart()
     elif "red" in hemotype or "crimson" in hemotype or "scarlet" in hemotype:
         print "Oh dear. How did the auxiliatrices miss a mutant?"
         dead("The quick jab of a culling fork corrects that problem.")
@@ -160,7 +169,106 @@ def abyss():
 
 ## LANDDWELLER PATH starts here ##
 
+def landstart():
+    print "Which way will you go?"
+    next = raw_input("> ")
+    
+    if "left" in next:
+        print "You have more sense than to go toward the light! That's always trouble."
+        print "Instead you head down the darker path, following its twists and turns."
+        print "When the tunnel opens out a little, you can see a small burrow in the wall."
+        print "There's a fleshy green cactus lobe on the cave floor."
+        goleft()
+    elif "right" in next:
+        print "You thrive on adventure. You're going to go see what this light is about."
+        print "You wiggle boldly into the next cave. There are two paths in front of "
+        print "you: the light is coming from the higher path, and looks like it must "
+        print "get quite bright up there. The lower path looks soothingly dark."
+        goright()
+    else:
+        print "You sit there, indecisive as a lump of mashed starchroot."
+        landstart()
 
+# Landdweller right branch
+
+def goright():
+    print "Climb up or crawl down?"
+    next = raw_input("> ")
+    
+    if "climb" in next or "up" in next:
+        print "You climb up toward the bright light. You emerge on a sunny patch of sand."
+        if "jade" in blood:
+            print "You bask in the warmth, chirring in delight at your discovery. The "
+            print "Virgin Mother Grub who had been waiting here for a suitable charge "
+            print "wakes at the sound of your tiny voice. She coos back at you and "
+            print "scoops you up in her feathery limbs. She'll take you in and nurture "
+            print "you, and when the time comes she'll teach you to nurture the grubs "
+            print "of the future."
+            exit(0)
+        else:
+            dead("Your unprotected body shrivels up and fries under the merciless sun.")
+    elif "crawl" in next or "down" in next:
+        print "Quite sensibly, you avoid the upward path. Adventure is one thing, but "
+        print "trolls were not made for sunshine! You trundle down the darker passage."
+        print "Hmm, what's that ahead of you?"
+        downright()
+    else:
+        print "You sit quietly, wondering if you could decide to be a rock and just not "
+        print "ever move from this spot."
+        goright()
+
+def downright():
+    print "Something... fluffy?"
+    next = raw_input("> ")
+    
+    if "y" in next:
+        # placeholder text
+        print "fennec ending"
+        exit(0)
+    else:
+        dead("A beak snaps in the darkness! A burrow owl won't go hungry tonight.")
+
+# Landdweller left branch
+
+def goleft():
+    print "You could peek into the burrow. You could keep exploring. You could even try"
+    print "eating the cactus. What will you do?"
+    next = raw_input("> ")
+    
+    if "burrow" in next:
+        # placeholder text
+        print "owl ending"
+        exit(0)
+    elif "cactus" in next:
+        print "That's got to be the strangest thing you've ever tasted. It makes you FEEL"
+        print "pretty strange, too."
+        dead("...Eventually, it makes you not feel anything at all.")
+    else:
+        print "You'll keep exploring, then. nothing around here looks that promising."
+        print "You follow the tunnel further. There's a heavy, meaty smell that gets "
+        print "stronger as you keep going. When you come to a point where the cave mouth"
+        print "widens, you can hear deep, growly snuffling from beyond."
+        explore()
+
+def explore():
+    print "Advance or abscond?"
+    next = raw_input("> ")
+    
+    if next == "advance":
+        print "You advance fearlessly into the lair of the beast."
+        # last node to be added here!
+    
+    elif next == "abscond":
+        print "You're not going anywhere near that obvious danger zone! Matter of fact,"
+        print "you're going all the way back to your starting point to consider what you"
+        print "should do next. You hurry back to the first cave: dark tunnel to the left,"
+        print "somewhat-lit tunnel to your right."
+        landstart()
+    else:
+        print "I don't think you were listening."
+        explore()
+
+    
 ## end of defs ##
 
 start()
